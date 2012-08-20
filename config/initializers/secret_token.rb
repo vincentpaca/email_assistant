e9272759cea49880313fbebdc3d6a15c3c640237
Vincent Paca <vincent.paca@gmail.com>
2012-08-20 11:23:35 +0800

    Initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailAssistant::Application.config.secret_token = 'a5d73d145370754d1d9bafce1622d2b983d1b7a4bc2231e86e55a458275372588af3d659e367d6a9cc231458a356116fa608bd13c9a0472d2ee04621e58b0608'
